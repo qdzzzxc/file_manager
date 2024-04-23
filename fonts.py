01701528c592d9216e0926ebe87a50fb5d3c8fa1
Nikita <nikitstp@gmail.com>
2024-04-23 14:14:18 +0300

    added 2 windows

---
+++ fonts.py
@@ -0,0 +1,5 @@
+import tkinter.font as tkFont
+
+common_font = tkFont.Font(family="Helvetica", size=12)
+selected_font = tkFont.Font(family="Helvetica", size=12, weight="bold")
+error_font = tkFont.Font(family="Helvetica", size=10)
--- main.py
+++ main.py
@@ -45,7 +45,7 @@ class ColumnPage(FilePage):
 #патерн адаптер
 class ColumnWindow:
     def __init__(self, root, width, height, data_page_cls, y_pad=20):
-        self.c = tk.Canvas(root, width, height, bg="lightblue", highlightthickness=0)
+        self.c = tk.Canvas(root, width=width, height=height, bg="lightblue", highlightthickness=0)
         self.data_page = data_page_cls()
 
         self.cursor_position = 0
@@ -55,55 +55,15 @@ class ColumnWindow:
         self.y_pad = y_pad
         self.files_on_page = (height - 10)//y_pad - 2
 
+        self.get_fonts()
 
-class Application(tk.Tk):
-    def __init__(self, width, height):
-        super().__init__()
-        self.title("File manager")
-        self.geometry(f"{width}x{height}")
-        self.width = width
-        self.height = height
-
-        self.cursor_position = 0
-        self.offset = 0
-        self.files_on_page = (HEIGHT - 10)//20 - 2
-        self.counters_history = []
-
-        self.f = tk.Frame(self, width=WIDTH, height=HEIGHT)
-        self.f.pack()
-        self.c = tk.Canvas(self.f, width=self.width//2, height=self.height, bg="lightblue", highlightthickness=0)
-        self.c2 = tk.Canvas(self.f, width=self.width//2, height=self.height, bg="lightblue", highlightthickness=10)
-        
-        self.c.grid(row=0, column=0)
-        self.c2.grid(row=0, column=1)
-
-        self.create_fonts()
-        self.bind_keys()
-
-        self.temp_window = ColumnPage()
-        self.draw_temp_state()
-
-    def bind_keys(self):
-        self.c.focus_set()
-        for letter in [
-            "s",
-            "S",
-            "w",
-            "W",
-            "Down",
-            "Up",
-            "Return",
-            "Escape",
-        ]:
-            self.c.bind(f"<KeyPress-{letter}>", self.on_key_press)
-
-    def create_fonts(self):
+    def get_fonts(self):
         self.common_font = tkFont.Font(family="Helvetica", size=12)
         self.selected_font = tkFont.Font(family="Helvetica", size=12, weight="bold")
         self.error_font = tkFont.Font(family="Helvetica", size=10)
 
     def draw_temp_state(self, highligted_color='black', higlighted_text=None):
-        content = self.temp_window.get_page_content()
+        content = self.data_page.get_page_content()
         if isinstance(content, dict):
             return content['error']
         self.draw_page(content, highligted_color=highligted_color, higlighted_text=higlighted_text)
@@ -124,52 +84,99 @@ class Application(tk.Tk):
             else:
                 self.c.create_text(10, y_offset, anchor="nw", text=text, font=font)
 
-            y_offset += 20
+            y_offset += self.y_pad
 
     def reset_counters(self):
-        self.counters_history.append((self.cursor_position, self.offset, self.temp_window.data_length))
+        self.counters_history.append((self.cursor_position, self.offset, self.data_page.data_length))
         self.cursor_position = 0
         self.offset = 0
 
     def get_counters(self):
         if self.counters_history:
-            self.cursor_position, self.offset, self.temp_window.data_length = self.counters_history.pop()
+            self.cursor_position, self.offset, self.data_page.data_length = self.counters_history.pop()
         else:
             self.cursor_position, self.offset = 0, 0 # self.temp_window.data_length всё равно какой
 
+
+class Application(tk.Tk):
+    def __init__(self, width, height):
+        super().__init__()
+        self.title("File manager")
+        self.geometry(f"{width}x{height}")
+        self.width = width
+        self.height = height
+
+        self.f = tk.Frame(self, width=WIDTH, height=HEIGHT)
+        self.f.pack()
+        self.left_window = ColumnWindow(self.f, width=self.width//2, height=self.height, data_page_cls=ColumnPage)
+        self.right_window = ColumnWindow(self.f, width=self.width//2, height=self.height, data_page_cls=ColumnPage)
+        
+        self.left_window.c.grid(row=0, column=0)
+        self.right_window.c.grid(row=0, column=1)
+
+        self.bind_keys()
+
+        self.left_window.draw_temp_state()
+        self.right_window.draw_temp_state()
+
+        self.temp_window = self.left_window
+
+    def bind_keys(self):
+        self.f.focus_set()
+        for letter in [
+            "s",
+            "S",
+            "w",
+            "W",
+            "Down",
+            "Up",
+            "Return",
+            "Escape",
+            "a",
+            "A",
+            "d",
+            "D",
+            "Left",
+            "Right",
+        ]:
+            self.f.bind(f"<KeyPress-{letter}>", self.on_key_press)
+
     def on_key_press(self, event):
         if event.keysym in ["Up", "w", "W"]:
-            self.cursor_position -= 1
+            self.temp_window.cursor_position -= 1
         if event.keysym in ["Down", "s", "S"]:
-            self.cursor_position += 1
+            self.temp_window.cursor_position += 1
+        if event.keysym in ["Left", "a", "A"]:
+            self.temp_window = self.left_window
+        if event.keysym in ["Right", "d", "D"]:
+            self.temp_window = self.right_window
         if event.keysym in ["Return"]:
-            if self.selected_file[1]:
-                self.temp_window.cd(self.selected_file[0])
-                self.reset_counters()
+            if self.temp_window.selected_file[1]:
+                self.temp_window.data_page.cd(self.temp_window.selected_file[0])
+                self.temp_window.reset_counters()
         if event.keysym in ["Escape"]:
-            check_head = self.temp_window.cd_higher()
+            check_head = self.temp_window.data_page.cd_higher()
             if check_head:
-                self.get_counters()
+                self.temp_window.get_counters()
 
         # self.cursor_position = min(max(0, self.cursor_position), self.temp_window.data_length - 1)
 
-        if self.cursor_position < 0:
-            self.cursor_position = self.temp_window.data_length - 1
-        if self.cursor_position > self.temp_window.data_length - 1:
-            self.cursor_position = 0
+        if self.temp_window.cursor_position < 0:
+            self.temp_window.cursor_position = self.temp_window.data_page.data_length - 1
+        if self.temp_window.cursor_position > self.temp_window.data_page.data_length - 1:
+            self.temp_window.cursor_position = 0
 
-        if self.cursor_position >= self.offset + self.files_on_page:
-            self.offset += self.cursor_position + 1 - self.offset - self.files_on_page
-            print(self.cursor_position + 1 - self.offset - self.files_on_page)
-        elif self.cursor_position < self.offset:
-            self.offset -= self.offset - self.cursor_position
+        if self.temp_window.cursor_position >= self.temp_window.offset + self.temp_window.files_on_page:
+            self.temp_window.offset += self.temp_window.cursor_position + 1 - self.temp_window.offset - self.temp_window.files_on_page
+        elif self.temp_window.cursor_position < self.temp_window.offset:
+            self.temp_window.offset -= self.temp_window.offset - self.temp_window.cursor_position
 
-        f = self.draw_temp_state()
+        f = self.temp_window.draw_temp_state()
         if f:
             print(f)
-            self.temp_window.cd_higher()
-            self.get_counters()
-            self.draw_temp_state(highligted_color='red', higlighted_text=f)
+            self.temp_window.data_page.cd_higher()
+            self.temp_window.get_counters()
+            self.temp_window.draw_temp_state(highligted_color='red', higlighted_text=f)
 
 
 # WIDTH = 600
